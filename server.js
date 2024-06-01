58f6fc71f921ceabb25cfaabf699504848922252
Kevin <kevin@gep.hu>
2024-06-01 16:49:06 +0200

    Change mongoose connect

--- server.js
+++ server.js
@@ -29,8 +29,6 @@ mongoose.promise = global.Promise;
 mongoose.set("strictQuery", false);
 mongoose
     .connect(URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
     })
     .then(console.log("Connected to database"))
     .catch((err) => console.log(err));
